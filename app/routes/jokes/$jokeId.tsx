395eac2b648d42b8a6b93eea67309a0d15f5f719
Alejo Boga <alejoboga20@gmail.com>
2023-02-02 11:26:48 -0500

    Fetching data

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,8 +1,26 @@
+import type { LoaderArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { db } from '~/utils/db.server';
+import { Link, useLoaderData } from '@remix-run/react';
+
+export const loader = async ({ params }: LoaderArgs) => {
+	const joke = await db.joke.findUnique({ where: { id: params.jokeId } });
+
+	if (!joke) {
+		throw new Error('joke not found');
+	}
+
+	return json({ joke });
+};
+
 const JokeRoute = () => {
+	const data = useLoaderData<typeof loader>();
+
 	return (
 		<div>
 			<p>Here's your hilarious joke:</p>
-			<p>Why don't you find hippopotamuses hiding in trees? They're really good at it.</p>
+			<p>{data.joke.content}</p>
+			<Link to='.'>{data.joke.name} Permalink</Link>
 		</div>
 	);
 };
--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,26 @@
+import { json } from '@remix-run/node';
+import { Link, useLoaderData } from '@remix-run/react';
+import { db } from '~/utils/db.server';
+
+export const loader = async () => {
+	const count = await db.joke.count();
+	const randomRowNumber = Math.floor(Math.random() * count);
+	const [randomJoke] = await db.joke.findMany({
+		take: 1,
+		skip: randomRowNumber,
+	});
+
+	return json({ randomJoke });
+};
+
 const JokesIndexRoute = () => {
+	const data = useLoaderData<typeof loader>();
+
 	return (
 		<div>
 			<p>Here's a random joke:</p>
-			<p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+			<p>{data.randomJoke.content}</p>
+			<Link to={data.randomJoke.id}>"{data.randomJoke.name}" Permalink</Link>
 		</div>
 	);
 };
