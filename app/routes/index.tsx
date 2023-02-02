e1b74bfd09a04961d433d0f416eb7bf028258932
Alejo Boga <alejoboga20@gmail.com>
2023-02-02 10:51:23 -0500

    Styling the app

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,28 @@
+import type { LinksFunction } from '@remix-run/node';
+import { Link } from '@remix-run/react';
+import stylesUrl from '~/styles/index.css';
+
+export const links: LinksFunction = () => {
+	return [{ rel: 'stylesheet', href: stylesUrl }];
+};
+
 const IndexRoute = () => {
-	return <div>IndexRoute</div>;
+	return (
+		<div className='container'>
+			<div className='content'>
+				<h1>
+					Remix <span>Jokes!</span>
+				</h1>
+				<nav>
+					<ul>
+						<li>
+							<Link to='jokes'>Read Jokes</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	);
 };
 
 export default IndexRoute;
--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,4 +1,11 @@
+import type { LinksFunction } from '@remix-run/node';
 import { Outlet } from '@remix-run/react';
+
+import stylesUrl from '~/styles/jokes.css';
+
+export const links: LinksFunction = () => {
+	return [{ rel: 'stylesheet', href: stylesUrl }];
+};
 
 const JokesRoute = () => {
 	return (
