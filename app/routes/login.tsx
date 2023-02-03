bafbde658515407512a042891c5fca1c7931f468
Alejo Boga <alejoboga20@gmail.com>
2023-02-03 14:05:54 -0500

    Create login and session

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -4,7 +4,7 @@ import { Link, useActionData, useSearchParams } from '@remix-run/react';
 import stylesUrl from '~/styles/login.css';
 import { db } from '~/utils/db.server';
 import { badRequest } from '~/utils/request.server';
-import { json } from '@remix-run/node';
+import { login, createUserSession } from '~/utils/session.server';
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesUrl }];
 
@@ -65,14 +65,19 @@ export const action = async ({ request }: ActionArgs) => {
 
 	switch (loginType) {
 		case 'login': {
+			const user = await login({ username, password });
+			console.log({ user });
+			if (!user) {
+				return badRequest({
+					fieldErrors: null,
+					fields,
+					formError: `Username/Password combination is incorrect`,
+				});
+			}
 			// login to get the user
 			// if there's no user, return the fields and a formError
 			// if there is a user, create their session and redirect to /jokes
-			return badRequest({
-				fieldErrors: null,
-				fields,
-				formError: 'Not implemented',
-			});
+			return createUserSession(user.id, redirectTo);
 		}
 		case 'register': {
 			const userExists = await db.user.findFirst({
