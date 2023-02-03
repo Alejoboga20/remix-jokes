96170a002709941d3bdbe64ccea9a941f3d90e71
Alejo Boga <alejoboga20@gmail.com>
2023-02-03 14:11:02 -0500

    Get user session

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -3,6 +3,7 @@ import { redirect } from '@remix-run/node';
 import { useActionData } from '@remix-run/react';
 import { db } from '~/utils/db.server';
 import { badRequest } from '~/utils/request.server';
+import { requireUserId } from '~/utils/session.server';
 
 const validateJokeContent = (content: string) => {
 	if (content.length < 10) {
@@ -17,6 +18,7 @@ const validateJokeName = (name: string) => {
 };
 
 export const action = async ({ request }: ActionArgs) => {
+	const userId = await requireUserId(request);
 	const form = await request.formData();
 	const name = form.get('name');
 	const content = form.get('content');
@@ -44,7 +46,7 @@ export const action = async ({ request }: ActionArgs) => {
 		});
 	}
 
-	const joke = await db.joke.create({ data: fields });
+	const joke = await db.joke.create({ data: { ...fields, jokesterId: userId } });
 
 	return redirect(`/jokes/${joke.id}`);
 };
